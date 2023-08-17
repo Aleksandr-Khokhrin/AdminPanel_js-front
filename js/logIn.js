ccf82dcfa2976576ed954cdafa9af7a3edb9c9f0
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-08-17 17:55:11 +0500

    fix

--- js/logIn.js
+++ js/logIn.js
@@ -1,24 +1,34 @@
-
 //boxes
 const autBox = document.getElementById('autBox')
 const regBox = document.getElementById('regBox')
 const mainBox = document.getElementById('mainBox')
+const mainBoxBody = document.querySelectorAll('.mainBoxBody .flexList')
+const mainBody = document.getElementById('mainBoxBody')
+// console.log(mainBoxBody.length)
 
 //inputs
-const emailAut = document.getElementById('emailAut')
+const nameAut = document.getElementById('nameAut')
 const keyAut = document.getElementById('keyAut')
+const emailReg = document.getElementById('emailReg')
+const nameReg = document.getElementById('nameReg')
+const keyRegOne = document.getElementById('keyRegOne')
+const keyRegTwo = document.getElementById('keyRegTwo')
 
 //buttons
 const getRegBox = document.getElementById('getRegBox')
 const getAutBox = document.getElementById('getAutBox')
 const autBtn = document.getElementById('autBtn')
+const regBtn = document.getElementById('regBtn')
+
 
 //span
 const userToken = document.getElementById('userToken')
+const userMainBoxName = document.getElementById('userMainBoxName')
+
 
 //listeners
 getRegBox.onclick = () => {
-    regBox.classList.remove('none')
+	regBox.classList.remove('none')
     autBox.classList.add('none')
 }
 getAutBox.onclick = () => {
@@ -29,30 +39,109 @@ getAutBox.onclick = () => {
 
 autBtn.onclick = function () {
 	let userInfo = {
-		username: emailAut.value,
+		username: nameAut.value,
 		password: keyAut.value,
 	};
+	userMainBoxName.textContent = nameAut.value
 	authUser(userInfo);
+}
+regBtn.onclick = function () {
+	let userInfo = {
+		username: nameReg.value,
+		password: keyRegOne.value,
+		email: emailReg.value
+	};
+	regUser(userInfo);
+}
+
+
+
+//Functions
+function showPassword() {
+	var key_attr = $('.key').attr('type');
+	if(key_attr != 'text') {
+		$('.checkbox').addClass('show');
+		$('.key').attr('type', 'text');
+	} else {
+		$('.checkbox').removeClass('show');
+		$('.key').attr('type', 'password');
+	}
+	
+}
+function UserFormToList (id, user, email, regDate, lastAct, state, index) {
+    return `
+	<div class="flexList" data-index="${index}">
+		<div class="col-list bg-info bg-opacity-10 border border-top-0 border-info"><input type="checkbox" data-index="${index}"></div>
+		<div data-index="${index}" class="col-list bg-info bg-opacity-10 border border-info border-start-0 border-top-0" style="overflow-wrap: break-word;">${id}</div>
+		<div data-index="${index}" class="col-list bg-info bg-opacity-10 border border-info border-start-0 border-top-0" style="overflow-wrap: break-word;">${user}</div>
+		<div data-index="${index}" class="col-list bg-info bg-opacity-10 border border-info border-start-0 border-top-0" style="overflow-wrap: break-word;">${email}</div>
+		<div data-index="${index}" class="col-list bg-info bg-opacity-10 border border-info border-start-0 border-top-0" style="overflow-wrap: break-word;">${regDate}</div>
+		<div data-index="${index}" class="col-list bg-info bg-opacity-10 border border-info border-start-0 border-top-0" style="overflow-wrap: break-word;">${lastAct}</div>
+		<div data-index="${index}" class="col-list bg-info bg-opacity-10 border border-info border-start-0 border-top-0" style="overflow-wrap: break-word;">${state}</div>
+	</div>`
+}
+
+
+
+//API
+async function regUser(userInfo) {
+	if(keyRegOne.value === '' || keyRegTwo.value === '' || nameReg.value === '' || emailReg.value === ''){
+		alert("Error! Enter all values");
+	}
+	if (keyRegTwo.value === keyRegOne.value) {
+		let response = await sendRequest("/registration", "POST", userInfo);
+		// console.log(response);
+		if (response) {
+			regBox.classList.add('none')
+			mainBox.classList.add('none')
+			autBox.classList.remove('none')
+		} else {
+			alert(response.message);
+		}
+	} else {
+		alert("Error! Passwords do not match!");
+	}
 }
 async function authUser(userInfo) {
 	let response = await sendRequest("/login", "POST", userInfo);
-	console.log(response);
-	if (response) {
+	// console.log(response);
+	if (response.message === "Пользователь user не найден") {
+		alert("Такого пользователя не существует!");
+	}else {
         regBox.classList.add('none')
         autBox.classList.add('none')
         mainBox.classList.remove('none')
-		userToken.textContent = `${response.token}`
+		getUsersInfo(response.token)
 
-	} else {
-		alert("Такого пользователя не существует!");
-	}
+	} 
+}
+async function getUsersInfo(token) {
+    let response = await sendRequestForBearer("/users", "GET", null, token); // Передаем токен как четвертый аргумент
+    // console.log(response);
+    console.log(response[0]._id);
+    console.log(response[0].username);
+    console.log(response[0].email);
+    console.log(response[0].lastActiveAt);
+    console.log(response[0].registeredAt);
+    console.log(response[0].roles);
+    console.log(response[0].password);
+    if (response) {
+		if (mainBoxBody.length === 0) {
+			mainBoxBody.innerHTML = '<h5 class="text-light">Список пуст</h5>'
+		}
+		for(let i = 0; i < response.length; i++) {
+			mainBody.insertAdjacentHTML("beforeend", UserFormToList (response[i]._id, response[i].username, response[i].email, response[i].registeredAt, response[i].lastActiveAt, response[i].roles[0], i))
+		}
+    } else {
+        alert('Error')
+    }
 }
 async function sendRequest(url, method, data) {
-	url = `https://bakerman-node-js-1fd874ff2f21.herokuapp.com/auth${url}`;
-	// url = `http://localhost:5000/auth${url}`;
+	// url = `https://bakerman-node-js-1fd874ff2f21.herokuapp.com/auth${url}`;
+	url = `http://localhost:5000/auth${url}`;
 
 	if (method == "POST") {
-        console.log(data);
+        // console.log(data);
 		let response = await fetch(url, {
 			method: "POST",
 			headers: {
@@ -77,22 +166,25 @@ async function sendRequest(url, method, data) {
 		return response;
 	}
 }
-
-
-function showPassword() {
-    
-    var key_attr = $('.key').attr('type');
-    
-    if(key_attr != 'text') {
-        
-        $('.checkbox').addClass('show');
-        $('.key').attr('type', 'text');
-        
-    } else {
-        
-        $('.checkbox').removeClass('show');
-        $('.key').attr('type', 'password');
-        
+async function sendRequestForBearer(url, method, data, token = null) {
+    // url = `https://bakerman-node-js-1fd874ff2f21.herokuapp.com/auth${url}`;
+    url = `http://localhost:5000/auth${url}`;
+    const headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    };
+    if (token) {
+        headers.Authorization = `Bearer ${token}`;
     }
-    
+    const options = {
+        method: method,
+        headers: headers,
+    };
+    if (data) {
+        options.body = JSON.stringify(data);
+    }
+    let response = await fetch(url, options);
+    response = await response.json();
+    return response;
 }
+
